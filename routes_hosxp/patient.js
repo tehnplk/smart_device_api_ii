8da8fd771d7105fbba638a30015b467c621a9741
tehnplk <tehnplk@gmail.com>
2022-08-18 10:24:34 +0700

    tehnn

--- routes_hosxp/patient.js
+++ routes_hosxp/patient.js
@@ -11,7 +11,7 @@ data_none = {
 
 router.get('/get_patient_by_cid/:cid', async function (req, res, next) {
   cid = req.params.cid
-  sql = `select hn,cid,concat(pname,fname,' ',lname) fullname
+  sql = `select hn,cid,concat(pname,fname,' ',lname) fullname,sex
   ,(select vn from vn_stat where vstdate = CURRENT_DATE  and cid = '${cid}' order by vn DESC limit 1) vn 
   from patient where cid = '${cid}' limit 1`
   r = await knex.raw(sql)
@@ -24,6 +24,7 @@ router.get('/get_patient_by_cid/:cid', async function (req, res, next) {
     'hn': r[0][0].hn,
     'cid': r[0][0].cid,
     'fullname': r[0][0].fullname,
+    'sex':r[0][0].sex,
     'vn': r[0][0].vn
   }
   res.json(data)
@@ -31,7 +32,7 @@ router.get('/get_patient_by_cid/:cid', async function (req, res, next) {
 
 router.get('/get_patient_by_hn/:hn', async function (req, res, next) {
   hn = req.params.hn
-  sql = `select hn,cid,concat(pname,fname,' ',lname) fullname
+  sql = `select hn,cid,concat(pname,fname,' ',lname) fullname,sex
   ,(select vn from vn_stat where vstdate = CURRENT_DATE  and hn = '${hn}' order by vn DESC limit 1) vn 
   from patient where hn = '${hn}' limit 1`
   r = await knex.raw(sql)
@@ -44,6 +45,7 @@ router.get('/get_patient_by_hn/:hn', async function (req, res, next) {
     'hn': r[0][0].hn,
     'cid': r[0][0].cid,
     'fullname': r[0][0].fullname,
+    'sex':r[0][0].sex,
     'vn': r[0][0].vn
   }
   res.json(data)
@@ -51,7 +53,7 @@ router.get('/get_patient_by_hn/:hn', async function (req, res, next) {
 
 router.get('/get_patient_by_vn/:vn', async function (req, res, next) {
   vn = req.params.vn
-  sql = `SELECT t.hn,p.cid,concat(p.pname,p.fname,' ',p.lname) fullname,t.vn 
+  sql = `SELECT t.hn,p.cid,concat(p.pname,p.fname,' ',p.lname) fullname,p.sex,t.vn 
   from ovst t INNER JOIN patient p ON t.hn = p.hn
   WHERE t.vn = '${vn}'`
   r = await knex.raw(sql)
@@ -64,6 +66,7 @@ router.get('/get_patient_by_vn/:vn', async function (req, res, next) {
     'hn': r[0][0].hn,
     'cid': r[0][0].cid,
     'fullname': r[0][0].fullname,
+    'sex':r[0][0].sex,
     'vn': r[0][0].vn
   }
   res.json(data)
