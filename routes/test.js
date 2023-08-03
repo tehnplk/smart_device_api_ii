6721511b33d3c4f6bafd7b934afb3137cc944ba4
tehnplk <tehnplk@gmail.com>
2023-08-03 09:15:14 +0700

    tehnnn

--- routes/test.js
+++ routes/test.js
@@ -1,31 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-var token = "2452cac74071f1d643f636358527354628cf2ca187121f36b43ff62b0ffda149f3938328d5ad5ae7d155e40df9ff4c5f9e224a51376e377a6d698c97aab84697"
 
 router.get('/send', async function (req, res, next) {
-
-    device_data = {
-        "bps": 120,
-        "bps": 80,
-        "pulse": 60,
-        "temp": 37.5
-    }
+    var token = "2452cac74071f1d643f636358527354628cf2ca187121f36b43ff62b0ffda149f3938328d5ad5ae7d155e40df9ff4c5f9e224a51376e377a6d698c97aab84697"
+    //var token = "xx"
     body_data = {
-        "cid":1111111111111,
-        "vn":0,
-        "device_data":device_data
+        "cid": "1111111111111",
+        "vn": 999999999,
+        "device_data": { bps: 139, bpd: 99, pulse: 66, temp: 37.9 }
     }
-    n = await axios.post('https://www.ihealthrawae.net:8080/send', body_data, {
+    n = await axios.post('https://ihealthrawae.net:8080/api/smart/send', body_data, {
         headers: {
-            'Authorization': `Basic ${token}`
+            'Authorization': `${token}`
         }
     })
-    console.log(n)
-    res.json({ 'n': "1" })
+    console.log("response status", n.status)
+    console.log(n.data)
+    res.json({ 'status': body_data.device_data })
 });
-
-
-
 
 module.exports = router;
