e388cb8ca89ec33ff0e57a3f105d760fc150b468
tehnplk <tehnplk@gmail.com>
2023-08-03 15:00:34 +0700

    tehnnn

--- routes/test.js
+++ routes/test.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+var config = require('../config.json')
 
 router.get('/send', async function (req, res, next) {
-    var token = "2452cac74071f1d643f636358527354628cf2ca187121f36b43ff62b0ffda149f3938328d5ad5ae7d155e40df9ff4c5f9e224a51376e377a6d698c97aab84697"
-    body_data = {
+     body_data = {
         "cid": "1111111111111",
         "vn": 999999999,
         "device_data": { bps: 139, bpd: 99, pulse: 66, temp: 37.9 }
     }
-    n = await axios.post('https://ihealthrawae.net:8080/api/smart/send', body_data, {
+    n = await axios.post(config.ihealth_api, body_data, {
         headers: {
-            'Authorization': `${token}`
+            'Authorization': `${config.ihealth_token}`
         }
     })
     console.log("response status", n.status)
