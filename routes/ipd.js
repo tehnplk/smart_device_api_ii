3361aec1002b1e4b63ea603c4b64008d1d2c4f13
tehnplk <tehnplk@gmail.com>
2024-03-26 10:24:14 +0700

    tehnn

--- routes/ipd.js
+++ routes/ipd.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../con_db')
+var knex = require('../con_db');
+var moment = require('moment')
 var config = require('../config.json')
+var bmsgw = require('../bmsgw.json')
+var knex_gw = require('../con_db_bmsgw')
+const axios = require('axios');
 
 data_none = {
   'hn': NaN,
@@ -68,7 +72,7 @@ router.get('/get_patient_by_hn/:hn', async function (req, res, next) {
   sql = `select hn,cid,concat(pname,fname,' ',lname) fullname,sex,birthday as birth
   ,(select an from an_stat where hn = '${hn}' order by an DESC limit 1) an
   from patient where hn = '${hn}' limit 1`
-  
+
 
 
   try {
@@ -154,8 +158,8 @@ router.post('/post_data', async function (req, res, next) {
   data = req.body
   console.log(data)
   if (config.mode_test) {
-    res.json(data)
-    return false
+    //res.json(data)
+    //return false
   }
 
   var _now = moment().format('YYYYMMDDHHmmss')
@@ -163,30 +167,33 @@ router.post('/post_data', async function (req, res, next) {
   console.log(data)
   if (config.not_post_if_null_pt & !data.hn) {
     console.log({ 'hn': 'no hn', 'data': data.data })
-    res.json({ 'hn': 'no hn', 'data': data.data })
-    return;
+    //res.json({ 'hn': 'no hn', 'data': data.data })
+    //return;
   }
 
+  let hl7 = `MSH|^~\\&|${data.data.machine}|${bmsgw.company}|HIS|BMS-HOSxP|${_now}||ORU^R01|2701|P|2.3\r\n`
+            hl7 = hl7 + `PID|1||${data.hn}|\r\n`
+            hl7 = hl7 + `PV1||I|||${data.an}||||||||||||||\n`
+            hl7 = hl7 + `OBR|1|||||${_now}||||||||${_now}\r\n`
+            hl7 = hl7 + `OBX|4|ST|TEMP||${data.data.tp}|C|||||F|||${_now}\r\n`
+            hl7 = hl7 + `OBX|5|ST|SYSTOLIC||${data.data.bps}|mmHg|||||F|||${_now}\r\n`
+            hl7 = hl7 + `OBX|6|ST|DIASTOLIC||${data.data.bpd}|mmHg|||||F|||${_now}\r\n`
+            hl7 = hl7 + `OBX|7|ST|PULSE||${data.data.press_pulse}|bpm|||||F|||${_now}\r\n`
+
+  console.log(hl7)
+
+  raw_data = {
+    'scn_result_receive_status': 'N',
+    'scn_result_stamp_datetime': _now,
+    'scn_result_receive_datetime': _now,
+    'scn_result_data': hl7,
+    'scn_result_no': data.hn,
+    'scn_identify_patient_type': 'hn',
+    'scn_result_msg_type': 'ORU^R01'
+  }
 
   try {
-    let hl7 = `MSH|^~\\&|${data.data.machine}|${bmsgw.company}|HIS|BMS-HOSxP|${_now}||ORU^R01|2701|P|2.3\r\n`
-    hl7 = hl7 + `PID|1||${data.hn}|\r\n`
-    hl7 = hl7 + `PV1|||||||||||||||||||\n`
-    hl7 = hl7 + `OBR|1|||||${_now}||||||||${_now}\r\n`
-    hl7 = hl7 + `OBX|1|ST|WEIGHT||${data.data.bw}|Kg|||||F|||${_now}\r\n`
-    hl7 = hl7 + `OBX|2|ST|HEIGHT||${data.data.bh}|cm|||||F|||${_now}\r\n`
-    hl7 = hl7 + `OBX|3|ST|BMI||${data.data.bmi}|kb/m2|||||F|||${_now}\r\n`
 
-
-    raw_data = {
-      'scn_result_receive_status': 'N',
-      'scn_result_stamp_datetime': _now,
-      'scn_result_receive_datetime': _now,
-      'scn_result_data': hl7,
-      'scn_result_no': data.hn,
-      'scn_identify_patient_type': 'hn',
-      'scn_result_msg_type': 'ORU^R01'
-    }
     r = await knex_gw('scn_result').insert(raw_data)
     res.json(r)
 
