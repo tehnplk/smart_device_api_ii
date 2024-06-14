2e30f0f762e03b5cb3f113db60c4e58e5ac7da68
tehnplk <tehnplk@gmail.com>
2024-06-14 14:30:51 +0700

    tehnn

--- routes/sp.js
+++ routes/sp.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var knex = require('../con_db')
 var moment = require('moment')
 var config = require('../config.json')
+var bmsgw = require('../bmsgw.json')
+var knex_gw = require('../con_db_bmsgw');
+const { is } = require('express/lib/request');
+const axios = require('axios');
 
 
 
@@ -14,6 +18,22 @@ router.post('/post_data_sp', async function (req, res, next) {
 
     if (config.mode_test) {
         res.json(data)
+        return false
+    }
+
+    if (config.his == 'ihealth') {
+        body_data = {
+            "cid": data.cid,
+            "vn": data.vn,
+            "device_data": { spo2: data.data.sp ,pulse:data.data.pulse }
+        }
+        n = await axios.post(`${config.ihealth_api}`, body_data, {
+            headers: {
+                'Authorization': `${config.ihealth_token}`
+            }
+        })
+        console.log("iHealth Response ", n.status)
+        res.json(n.status, data)
         return false
     }
 
--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 exports.version = function () {
-  return "II-3.2.3 (2024-05-06)(BMSGW/OPD/IPD/SOS)";
+  return "II-3.2.4 (2024-06-14)(BMSGW/OPD/IPD/SOS)";
 };
